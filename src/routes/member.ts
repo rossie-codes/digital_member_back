046df929b928ef0ae05657802c3d8dd65492b984
Alan <cf.alanpang@gmail.com>
2024-10-16 23:14:45 +0800

    try to solve cors problem

--- src/routes/member.ts
+++ src/routes/member.ts
@@ -15,9 +15,13 @@ const memberRouter = new Hono();
 // GET /member - Retrieve all members
 memberRouter.get('/get_member_list', async (c: Context) => {
     try {
-        const data = await getMemberList();
+      console.log('get_member_list route begin');
+      console.log(process.env.ALLOWED_ORIGINS) 
+      const data = await getMemberList();
+      console.log('get_member_list route done');
         return c.json(data);
     } catch (error) {
+      console.log('get_member_list route end in error');
         // Let Hono’s `onError` handle the error
         throw error;
     }
